6408ad4c0ab4faee43fa946e38b4d9231f4ce610
Chris <hizyyv@gmail.com>
2023-12-20 00:23:01 +0800

    feat: add client.d.ts

--- playground/app.vue
+++ playground/app.vue
@@ -1,13 +1,21 @@
 <script lang='ts' setup>
-console.log(import.meta.env.__UNO__)
+console.log(__UNO__)
 console.log(import.meta.env.__UNO_THEME__)
 
-console.log((import.meta.env.__UNO__))
-console.log(Object.keys(import.meta.env.__UNO__))
+console.log(import.meta.env.__UNO__)
+
+const presets = __UNO_CONFIG__.presets?.map((p: any)=>p.name)
+const transformers = import.meta.env.__UNO__.userConfig.transformers?.map((t: any)=>t.name)
+
 </script>
 
 <template>
   <div class="text-white">
-    111
+    <ul>
+      <li v-for="name in presets" :key="name">{{ name }}</li>
+    </ul>
+    <ul>
+      <li v-for="name in transformers" :key="name">{{ name }}</li>
+    </ul>
   </div>
 </template>
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,6 @@
 export interface Options {
+  /**
+   * Uno config path
+   */
   path?: string
-  dts?: string | boolean
 }
